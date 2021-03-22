84a8f9b3b1c05df718becf7892c1f3cb824efb70
daonham <daonham95@gmail.com>
2021-03-22 12:02:52 +0700

    fix: operation in typography

--- src/components/typopgraphy/style.js
+++ src/components/typopgraphy/style.js
@@ -2,12 +2,12 @@ export default function GridHubStyleTypography( { font, fontSize, lineHeight, fo
 
 	return {
 		'font-family': font ? ( font.indexOf( ' ' ) >= 0 ? `"${font}"` : font ) : undefined,
-		'font-size': fontSize ? fontSize[ device ] : undefined,
-		'line-height': lineHeight ? lineHeight[ device ] : undefined,
+		'font-size': fontSize?.[ device ],
+		'line-height': lineHeight?.[ device ],
 		'font-weight': fontWeight ? ( fontWeight === 'regular' ? 400 : fontWeight ) : undefined,
 		'text-decoration': decoration || undefined,
 		'text-transform': transform || undefined,
 		'font-style': fontStyle || undefined,
-		'letter-spacing': ( letterSpacing && letterSpacing[ device ] !== undefined && letterSpacing[ device ] !== null ) ? `${letterSpacing[ device ]}px` : undefined,
+		'letter-spacing': letterSpacing?.[ device ] ? `${letterSpacing[ device ]}px` : undefined,
 	};
 }
